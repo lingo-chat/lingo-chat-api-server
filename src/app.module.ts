a2afefb0cfdf09a22a9691d81f1983009874b26b
heaseung123 <showui96@gmail.com>
2024-07-09 15:10:01 +0900

    feat: users, auth 모듈 추가
    feat-#7

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { validationSchema } from './global/configs/validation.schema';
+import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import jwtConfiguration from './global/configs/jwt.configuration';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			cache: true,
+			load: [jwtConfiguration],
+			envFilePath: `.${process.env.NODE_ENV}.env`,
+			validationSchema,
+		}),
+		DatabaseModule,
+		UsersModule,
+		AuthModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
