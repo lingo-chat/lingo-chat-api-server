11a4432cb2c861c4997c4df149a57a70c3f80673
heaseung123 <showui96@gmail.com>
2024-07-12 14:34:00 +0900

    feat: response interceptor
    - response mapping
    - interceptors global 적용
    - res decorator 생성 및 response message 정의
    
    feat-#11

---
+++ src/auth/classes/auth.response.message.ts
@@ -0,0 +1,6 @@
+export class AuthReponseMessage {
+	static SIGN_UP = '회원 가입 성공';
+	static LOG_IN = '로그인 성공';
+	static LOG_OUT = '로그아웃 성공';
+	static REFRESH = '토큰 새로고침 성공';
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { TransformInterceptor } from './global/interceptors/transform.interceptor';
+import { ClassSerializerInterceptor } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	const configService = app.get(ConfigService);
 	const PORT = configService.getOrThrow('SERVER_PORT');
+	const reflector = new Reflector();
+
+	app.useGlobalInterceptors(new TransformInterceptor(reflector), new ClassSerializerInterceptor(reflector));
 
 	await app.listen(PORT);
 }
