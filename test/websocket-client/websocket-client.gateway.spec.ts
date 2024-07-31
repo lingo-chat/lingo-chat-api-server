b22a014b3df615d50d27a9eab0a397c67a22d29a
heaseung123 <showui96@gmail.com>
2024-07-31 16:51:58 +0900

    test: init websocket-client

--- test/websocket-client/websocket-client.gateway.spec.ts
+++ test/websocket-client/websocket-client.gateway.spec.ts
@@ -1,18 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { WebsocketClientGateway } from './websocket-client.gateway';
+import { WebsocketClientGateway } from '../../src/websocket-client/websocket-client.gateway';
 
 describe('WebsocketClientGateway', () => {
-  let gateway: WebsocketClientGateway;
+	let gateway: WebsocketClientGateway;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [WebsocketClientGateway],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [WebsocketClientGateway],
+		}).compile();
 
-    gateway = module.get<WebsocketClientGateway>(WebsocketClientGateway);
-  });
+		gateway = module.get<WebsocketClientGateway>(WebsocketClientGateway);
+	});
 
-  it('should be defined', () => {
-    expect(gateway).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(gateway).toBeDefined();
+	});
 });
