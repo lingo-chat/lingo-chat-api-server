83edea02eb35ea2ea4f43cb67a19284553140447
heaseung123 <showui96@gmail.com>
2024-08-24 13:52:21 +0900

    feat: 상세 채팅 기능
    - AI서버로부터 응답 임시 코드 -> 실제로 채팅이 가능하도록 레디스큐에 채팅데이터 추가하는 것으로 수정
    - 실시간 채팅방 리스트 업데이트를 위한 신규 채팅방 조회 및 이벤트 발생
    
    feat-#19

--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -7,60 +7,39 @@ import { PersonaService } from 'src/persona/persona.service';
 import { UsersService } from 'src/users/users.service';
 import { personaList } from 'src/persona/entities/personaList.entity';
 import { User } from 'src/users/entities/user.entity';
+import { RedisStorageService } from 'src/redis/redis-storage.service';
 
 @Injectable()
 export class ChatsService {
 	constructor(
 		private readonly personaService: PersonaService,
 		private readonly usersService: UsersService,
+		private readonly websocketClientGateway: WebsocketClientGateway,
 		@InjectRepository(ChatRoom)
 		private readonly chatRoomRepository: Repository<ChatRoom>,
+		private readonly redisStorageService: RedisStorageService,
 	) {}
 
-	async processChatMessage(providerId: string, message: string, personaId: number) {
+	async processFistChatMessage(provideId: string, message: string, personaId: number) {
 		const persona = await this.personaService.getPersona(personaId);
 		const personaName = persona.name;
 
 		const title = await this.createChatTitle(message, personaName);
-		const chatRoom = await this.createChatRoom(providerId, title, persona);
+		const chatRoom = await this.createChatRoom(provideId, title, persona);
+		const newChatRoom = await this.findNewChatRoom(chatRoom.id);
 
-		// AI 서버로부터 답변 받아오는 임시 코드
-		const url = 'https://fa9e-121-130-44-227.ngrok-free.app/v1/chat/completions';
-		const apiKey = 'test_api_key';
-		const sendData = {
-			model: '/home/iwbaporandhh/huggingface/models/llama-3-ko-8B-science-chat',
-			messages: [
-				{ role: 'system', content: '너의 이름은 궤도입니다.' },
-				{ role: 'user', content: '안녕하세요, 테스트 메세지입니다. 당신의 이름은 무엇인가요?' },
-			],
-			temperature: 0.6,
-			top_p: 0.9,
-			frequency_penalty: 1.4,
-			max_tokens: 2048, // == completion tokens
-			seed: 42,
-			stream: false,
-			stop: ['</s>'],
-		};
+		this.websocketClientGateway.sendNewChatRoom('created_new_chat_room', {
+			userId: provideId,
+			newChatRoom,
+		});
 
-		await fetch(url, {
-			method: 'POST',
-			headers: {
-				Authorization: `Bearer ${apiKey}`,
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(sendData),
-		})
-			.then((response) => response.json())
-			.then((result) => {
-				if (result.choices && result.choices.length > 0) {
-					console.log(result.choices[0].message.content);
-				} else {
-					console.error('Unexpected response structure:', result);
-				}
-			})
-			.catch((error) => {
-				console.error('Error:', error);
-			});
+		const user_id = provideId;
+		const chat_room_id = chatRoom.id;
+		const user_message = message;
+
+		const str = JSON.stringify({ user_id, chat_room_id, user_message });
+
+		await this.redisStorageService.queue('user_ms_queue', str);
 
 		return {
 			chatRoomId: chatRoom.id,
@@ -73,8 +52,8 @@ export class ChatsService {
 		return personaName + `-` + truncatedMessage;
 	}
 
-	private async createChatRoom(providerId: string, title: string, persona: personaList) {
-		const user = await this.usersService.findUser({ provide_id: providerId });
+	private async createChatRoom(provideId: string, title: string, persona: personaList) {
+		const user = await this.usersService.findUser({ provide_id: provideId });
 
 		const newChatRoom = {
 			title: title,
@@ -83,6 +62,50 @@ export class ChatsService {
 		};
 
 		return await this.chatRoomRepository.save(newChatRoom);
+	}
+
+	// AI 서버로부터 답변 받아오는 임시 코드
+	async responseAIserverTest(): Promise<string | null> {
+		const url = '';
+		const apiKey = '';
+		const sendData = {
+			model: '/home/iwbaporandhh/huggingface/models/llama-3-ko-8B-science-chat',
+			messages: [
+				{ role: 'system', content: '너의 이름은 궤도입니다.' },
+				{ role: 'user', content: '안녕하세요, 테스트 메세지입니다. 당신의 이름은 무엇인가요?' },
+			],
+			temperature: 0.6,
+			top_p: 0.9,
+			frequency_penalty: 1.4,
+			max_tokens: 2048,
+			seed: 42,
+			stream: false,
+			stop: ['</s>'],
+		};
+
+		try {
+			const response = await fetch(url, {
+				method: 'POST',
+				headers: {
+					Authorization: `Bearer ${apiKey}`,
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(sendData),
+			});
+
+			const result = await response.json();
+
+			if (result.choices && result.choices.length > 0) {
+				const message = result.choices[0].message.content;
+				return message; // 메시지를 반환합니다.
+			} else {
+				console.error('Unexpected response structure:', result);
+				return null;
+			}
+		} catch (error) {
+			console.error('Error:', error);
+			return null;
+		}
 	}
 
 	async findChatRooms(user: User) {
@@ -94,5 +117,14 @@ export class ChatsService {
 		});
 
 		return chatRooms;
+	}
+
+	async findNewChatRoom(chatRoomid: number) {
+		const newChatRoom = await this.chatRoomRepository.findOne({
+			where: { id: chatRoomid },
+			relations: ['persona'],
+		});
+
+		return newChatRoom;
 	}
 }
